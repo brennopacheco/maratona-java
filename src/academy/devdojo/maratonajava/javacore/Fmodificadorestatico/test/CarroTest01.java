3e1c6bde7a39caebea7f25d36669fae42e1e981f
brennopacheco <brenno_pacheco@hotmail.com>
2022-06-15 21:16:51 -0300

    UPDATE

--- src/academy/devdojo/maratonajava/javacore/Fmodificadorestatico/test/CarroTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Fmodificadorestatico/test/CarroTest01.java
@@ -3,17 +3,22 @@ package academy.devdojo.maratonajava.javacore.Fmodificadorestatico.test;
 import academy.devdojo.maratonajava.javacore.Fmodificadorestatico.domain.Carro;
 
 public class CarroTest01 {
+    /*
+    * Modificador de acesso estático - irá fazer o atributo pertencer à classe
+    * e todos os objetos vão compartilhar o mesmo valor.
+    * */
     public static void main(String[] args) {
+        Carro.setVelocidadeLimite(180); // estamos mudando a velocidadeLimite para todos as instâncias da classe Carro
+
         Carro c1 = new Carro("BMW", 280);
         Carro c2 = new Carro("Mercedes", 275);
         Carro c3 = new Carro("Audi", 290);
 
-//        c1.setVelocidadeLimite(180);
-        Carro.velocidadeLimite = 180;
+//        c1.setVelocidadeLimite(180); estariamos mudando a velocidade limite para apenas 1 objeto
 
-        c1.imprie();
-        c2.imprie();
-        c3.imprie();
+        c1.imprime();
+        c2.imprime();
+        c3.imprime();
 
     }
 }
